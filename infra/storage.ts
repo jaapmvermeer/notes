6a582c5b17c11a450a2cb2a765272be74c24afac
Jaap Vermeer <jaapmvermeer@gmail.com>
2024-08-15 14:35:11 -0400

    Adding a billing API

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,6 +1,7 @@
 // Create an S3 bucket
 export const bucket = new sst.aws.Bucket("Uploads");
-
+// Create a secret for Stripe
+export const secret = new sst.Secret("StripeSecretKey");
 // Create the DynamoDB table
 export const table = new sst.aws.Dynamo("Notes", {
   fields: {
